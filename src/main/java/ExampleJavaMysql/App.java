86cdb74f0c58968fd50e430f2745c91806e8d13c
Gustavo L <freazesss@gmail.com>
2020-11-08 00:25:21 -0300

    :sparkles: feat: pushing the Files

---
+++ src/main/java/ExampleJavaMysql/App.java
@@ -0,0 +1,18 @@
+package ExampleJavaMysql;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/"
+                                                            + "?user=root&password=root");
+            Db.ExecuteSql(conn);
+        } catch(SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ src/main/java/ExampleJavaMysql/Db.java
@@ -0,0 +1,37 @@
+package ExampleJavaMysql;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class Db {
+    public static void ExecuteSql(Connection connection) {
+        Statement smtp = null;
+        ResultSet rs = null;
+        try {
+            smtp = connection.createStatement();
+            smtp.executeUpdate("use TESTE");
+            smtp.executeUpdate(
+                    "INSERT INTO Testando (blabla)"
+                            +"values (1)");
+            rs = smtp.getResultSet();
+            System.out.println(rs);
+        } catch(SQLException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) { }
+                rs = null;
+            }
+
+            if (smtp != null) {
+                try {
+                    smtp.close();
+                } catch (SQLException sqlEx) { }
+                smtp = null;
+            }
+        }
+    }
+}
